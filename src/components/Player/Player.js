384eda64fca268c62da713d4f4e98eabf2f91ec6
Tuzbay <t.uzbayyelce@gmail.com>
2023-02-17 01:05:05 +0300

    Components templates added

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Player.css";
+import Sidebar from "../Sidebar/Sidebar";
+import Body from "../Body/Body";
+import Footer from "../Footer/Footer";
 
 function Player() {
   return (
     <div className="player">
-      <div className="player__body"></div>
+      <div className="player__body">
+        <Sidebar />
+        <Body />
+      </div>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import SidebarOption from "../SidebarOption/SidebarOption";
+import "./Sidebar.css";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
+    </div>
+  );
+}
+
+export default Sidebar;
