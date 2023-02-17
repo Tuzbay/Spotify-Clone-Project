387ff1e05ec7fad46f39c779da2c03ed6030adb5
Tuzbay <t.uzbayyelce@gmail.com>
2023-02-17 15:21:21 +0300

    Footer Component Added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,50 @@
 import React from "react";
 import "./Footer.css";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
+import { Grid, Slider } from "@mui/material";
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>I am the footer!</h1>
+      <div className="footer__left">
+        <img
+          className="footer__albumLogo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT19jWs2LO1yepUxYI76w6fcDgtzMRLSSBQtA&usqp=CAU"
+          alt=""
+        />
+        <div className="footer__songInfo">
+          <h4>Me Gustos Tu!</h4>
+          <p>Manu Chao</p>
+        </div>
+      </div>
+
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green />" />
+      </div>
+
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
